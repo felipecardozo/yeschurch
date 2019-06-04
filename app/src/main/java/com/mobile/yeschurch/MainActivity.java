2d778bdcc1ad2bbe121c2ce60ca6a85deb5913d8
Felipe Cardozo <pypelyncar@hotmail.com>
2019-06-03 21:40:05 -0500

    add maps activity and drtails button

--- app/src/main/java/com/mobile/yeschurch/MainActivity.java
+++ app/src/main/java/com/mobile/yeschurch/MainActivity.java
@@ -1,13 +1,32 @@
 package com.mobile.yeschurch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mapsButtons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mapsButtons = (Button) findViewById(R.id.buttonMaps);
+
+        mapsButtons.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nextActivity(MapsActivity.class);
+            }
+        });
+    }
+
+    private void nextActivity(Class clazz){
+        Intent intent = new Intent(this, clazz.getClass());
+        startActivity(intent);
     }
 }
