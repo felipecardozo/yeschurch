63e20f245afa5088c29665f176fe9449d0530a66
Paola Zarate GeekGirl <paolazaratem@gmail.com>
2019-06-09 00:33:01 -0500

    add all fields of details

--- app/src/main/java/com/mobile/yeschurch/DetailActivity.java
+++ app/src/main/java/com/mobile/yeschurch/DetailActivity.java
@@ -29,11 +29,43 @@ public class DetailActivity extends AppCompatActivity {
         Church church = createChurch();
         EditText nameChurch = (EditText)findViewById(R.id.text_church_name);
         nameChurch.setText(church.getName());
+
+        EditText webChurch = (EditText)findViewById(R.id.text_church_url);
+        webChurch.setText(church.getWebPage());
+
+        EditText assistanceChurch = (EditText)findViewById(R.id.text_church_assistants);
+        assistanceChurch.setText(church.getAssistance() + "");
+
+        EditText scheduleChurch = (EditText)findViewById(R.id.text_church_schedule);
+        scheduleChurch.setText(church.getSchedule().toString());
+
+        EditText kidsChurch = (EditText)findViewById(R.id.text_church_kids);
+        kidsChurch.setText(church.getHasKids().toString());
+
+        EditText hasParkingChurch = (EditText)findViewById(R.id.text_church_hasParking);
+        hasParkingChurch.setText(church.getHasParking().toString());
+
+        EditText hasParkingOutsideChurch = (EditText)findViewById(R.id.text_church_hasParkingOutside);
+        hasParkingOutsideChurch.setText(church.getHasParkingOutside().toString());
+
+        EditText hasAccessibilityChurch = (EditText)findViewById(R.id.text_church_hasAccessibility);
+        hasAccessibilityChurch.setText(church.getHasAccessibility().toString());
+
+        EditText idiomsChurch = (EditText)findViewById(R.id.text_church_idioms);
+        idiomsChurch.setText(church.getIdioms().toString());
+
+        EditText hasSignLanguageChurch = (EditText)findViewById(R.id.text_church_hasSignLanguage);
+        hasSignLanguageChurch.setText(church.getHasSignLanguage().toString());
+
+        EditText hasBathroomsChurch = (EditText)findViewById(R.id.text_church_hasBathrooms);
+        hasBathroomsChurch.setText(church.getHasBathrooms().toString());
     }
 
     private Church createChurch() {
         Church church = new Church();
-        church.setName("iglesia señor milagros");
+        church.setName("El lugar de su presencia");
+        church.setWebPage("www.supresencia.com");
+        church.setAssistance(500000);
 
         return church;
     }
