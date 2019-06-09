b03c77888692b926fb3652e897f631d55102d748
Paola Zarate GeekGirl <paolazaratem@gmail.com>
2019-06-09 17:38:51 -0500

    add Picture Acitivity

--- app/src/main/java/com/mobile/yeschurch/DetailActivity.java
+++ app/src/main/java/com/mobile/yeschurch/DetailActivity.java
@@ -1,14 +1,20 @@
 package com.mobile.yeschurch;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
+    private Button picturesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,14 @@ public class DetailActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        picturesButton = findViewById(R.id.button_church_photos);
+        picturesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nextActivity(PictureActivity.class);
             }
         });
 
@@ -67,7 +81,31 @@ public class DetailActivity extends AppCompatActivity {
         church.setWebPage("www.supresencia.com");
         church.setAssistance(500000);
 
+        List <String> schedules = new ArrayList<>();
+        schedules.add("Miercoles 7pm");
+        schedules.add("Martes y Jueves 6am-7am");
+
+        church.setSchedule(schedules);
+        church.setKids(true);
+        church.setParking(true);
+        church.setParkingOutside(false);
+        church.setAccessibility(true);
+
+        List <String> idioms = new ArrayList<>();
+        idioms.add("Ingles");
+        idioms.add("Español");
+        church.setIdioms(idioms);
+
+        church.setSignLanguage(true);
+        church.setBathrooms(true);
+
+
         return church;
+    }
+
+    private void nextActivity(Class clazz){
+        Intent intent = new Intent(this, clazz);
+        startActivity(intent);
     }
 
 }
