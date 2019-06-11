e875de676a2935e8ae6d32a9352a99599f1c2773
Felipe Cardozo <pypelyncar@hotmail.com>
2019-06-11 17:13:21 -0500

    few improvments detail act UI

--- app/src/main/java/com/mobile/yeschurch/DetailActivity.java
+++ app/src/main/java/com/mobile/yeschurch/DetailActivity.java
@@ -6,9 +6,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -50,26 +52,31 @@ public class DetailActivity extends AppCompatActivity {
         EditText scheduleChurch = (EditText)findViewById(R.id.text_church_schedule);
         scheduleChurch.setText(church.getSchedule().toString());
 
-        EditText kidsChurch = (EditText)findViewById(R.id.text_church_kids);
-        kidsChurch.setText( Boolean.toString(church.isKids()));
+        ImageView kidsChurch = (ImageView)findViewById(R.id.text_church_kids_yes);
+        //ImageView kidsChurchNo = (ImageView)findViewById(R.id.text_church_kids_no);
+        /*if( church.isKids()==false ){
+            Log.i("DETAILS", church.isKids()+"");
+            kidsChurchNo.setVisibility(ImageView.INVISIBLE);
+        }*/
 
-        EditText hasParkingChurch = (EditText)findViewById(R.id.text_church_hasParking);
-        hasParkingChurch.setText(Boolean.toString(church.isParking()));
 
-        EditText hasParkingOutsideChurch = (EditText)findViewById(R.id.text_church_hasParkingOutside);
-        hasParkingOutsideChurch.setText(Boolean.toString(church.isParkingOutside()));
+        ImageView hasParkingChurch = findViewById(R.id.text_church_hasParking_yes);
+        //hasParkingChurch.setText(Boolean.toString(church.isParking()));
 
-        EditText hasAccessibilityChurch = (EditText)findViewById(R.id.text_church_hasAccessibility);
-        hasAccessibilityChurch.setText(Boolean.toString(church.isAccessibility()));
+        ImageView hasParkingOutsideChurch = findViewById(R.id.text_church_hasParkingOutside_yes);
+        //hasParkingOutsideChurch.setText(Boolean.toString(church.isParkingOutside()));
+
+        ImageView hasAccessibilityChurch = findViewById(R.id.text_church_hasAccessibility_yes);
+        //hasAccessibilityChurch.setText(Boolean.toString(church.isAccessibility()));
 
         EditText idiomsChurch = (EditText)findViewById(R.id.text_church_idioms);
         idiomsChurch.setText(church.getIdioms().toString());
 
-        EditText hasSignLanguageChurch = (EditText)findViewById(R.id.text_church_hasSignLanguage);
-        hasSignLanguageChurch.setText(Boolean.toString(church.isSignLanguage()));
+        ImageView hasSignLanguageChurch = findViewById(R.id.text_church_hasSignLanguage_yes);
+        //hasSignLanguageChurch.setText(Boolean.toString(church.isSignLanguage()));
 
-        EditText hasBathroomsChurch = (EditText)findViewById(R.id.text_church_hasBathrooms);
-        hasBathroomsChurch.setText(Boolean.toString(church.isBathrooms()));
+        ImageView hasBathroomsChurch = findViewById(R.id.text_church_hasBathrooms_yes);
+        //hasBathroomsChurch.setText(Boolean.toString(church.isBathrooms()));
     }
 
     private Church createChurch() {
